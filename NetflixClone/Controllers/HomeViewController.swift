94564738e6675be6ea2a8e0f1b3d2ce5cf4e4788
Anastasiia Tereshchenko <ms.staisy@gmail.com>
2022-12-09 19:13:52 -0800

    0002: add Swiftlint and swift-format

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -13,16 +13,16 @@ class HomeViewController: UIViewController {
 		table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 		return table
 	}()
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.backgroundColor = .blue
 		view.addSubview(homeFeedTable)
-		
+
 		homeFeedTable.delegate = self
 		homeFeedTable.dataSource = self
 	}
-	
+
 	override func viewDidLayoutSubviews() {
 		super.viewDidLayoutSubviews()
 		homeFeedTable.frame = view.bounds
@@ -33,19 +33,19 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		20
 	}
-	
+
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
 																						 for: indexPath)
 		cell.textLabel?.text = "Hello"
-		
+
 		return cell
 	}
-	
+
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 		return 200
 	}
-	
+
 	private func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> Int {
 		return 40
 	}
